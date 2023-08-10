2269d312dc248d5ba8438b921a7e42ed9bdcc7f3
dlrjs2360 <lhg3670@naver.com>
2023-08-10 15:52:56 +0900

    :tada: setting: 프로젝트 생성

---
+++ src/main/java/com/springles/SpringlesApplication.java
@@ -0,0 +1,13 @@
+package com.springles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springles/SpringlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
