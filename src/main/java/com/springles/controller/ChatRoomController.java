f0a2425f409fcd8457f9c1d356ea2b5c585ab066
ujumom <amethyst270@naver.com>
2023-08-12 22:34:22 +0900

    ✨ feat: <controller, service 수정>

--- src/main/java/com/springles/controller/ChatRoomController.java
+++ src/main/java/com/springles/controller/ChatRoomController.java
@@ -1,12 +1,18 @@
 package com.springles.controller;
 
+import com.springles.domain.constants.ResponseCode;
 import com.springles.domain.dto.request.ChatRoomReqDTO;
+import com.springles.domain.dto.response.ResResult;
+import com.springles.exception.CustomException;
+import com.springles.exception.constants.ErrorCode;
 import com.springles.service.ChatRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,7 +26,23 @@ public class ChatRoomController {
     // 채팅방 생성
     @Operation(summary = "채팅방 생성", description = "채팅방 생성")
     @PostMapping(value = "/chatrooms", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Object createChatRoom(@Valid @RequestBody ChatRoomReqDTO chatRoomReqDTO){
-            return chatRoomService.createChatRoom(chatRoomReqDTO);
+    public ResponseEntity<ResResult> createChatRoom(@Valid @RequestBody ChatRoomReqDTO chatRoomReqDTO){
+        // request 자체가 빈 경우
+        if (chatRoomReqDTO == null) return ResponseEntity.badRequest().build();
+        // 비밀방 선택- 비밀번호 입력하지 않은 경우라면
+        if (!chatRoomReqDTO.getOpen() && chatRoomReqDTO.getPassword() == null) throw new CustomException(ErrorCode.PASSWORD_EMPTY);
+        // 제목이 비어있는 경우
+        if (chatRoomReqDTO.getTitle() == null) throw new CustomException(ErrorCode.TITLE_EMPTY);
+        // 인원이 범위를 벗어나는 경우
+        Long capacity = chatRoomReqDTO.getCapacity();
+        if (capacity < 5 || capacity > 10) throw new CustomException(ErrorCode.CAPACITY_WRONG);
+        // 응답 메시지 return
+        ResponseCode responseCode = ResponseCode.CHATROOM_CREATE;
+        return new ResponseEntity<>(ResResult.builder()
+                .responseCode(responseCode)
+                .code(responseCode.getCode())
+                .message(responseCode.getMessage())
+                .data(chatRoomService.createChatRoom(chatRoomReqDTO))
+                .build(), HttpStatus.OK);
     }
 }
--- src/main/java/com/springles/service/impl/ChatRoomServiceImpl.java
+++ src/main/java/com/springles/service/impl/ChatRoomServiceImpl.java
@@ -1,17 +1,11 @@
 package com.springles.service.impl;
 
-import com.springles.domain.constants.ResponseCode;
 import com.springles.domain.dto.request.ChatRoomReqDTO;
-import com.springles.domain.dto.response.ResResult;
 import com.springles.domain.entity.ChatRoom;
-import com.springles.exception.CustomException;
-import com.springles.exception.constants.ErrorCode;
 import com.springles.repository.ChatRoomJpaRepository;
 import com.springles.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import static com.springles.domain.dto.request.ChatRoomReqDTO.createToEntity;
@@ -24,25 +18,8 @@ public class ChatRoomServiceImpl implements ChatRoomService {
 
     @Transactional
     @Override
-    public Object createChatRoom(ChatRoomReqDTO chatRoomReqDTO) {
-        // 비밀방 선택 - 비밀번호 입력하지 않은 경우라면
-        if (!chatRoomReqDTO.getOpen() && chatRoomReqDTO.getPassword()==null) throw new CustomException(ErrorCode.PASSWORD_EMPTY);
-        // 제목이 비어있는 경우
-        if (chatRoomReqDTO.getTitle() == null) throw new CustomException(ErrorCode.TITLE_EMPTY);
-        // 인원이 범위를 벗어나는 경우
-        Long capacity = chatRoomReqDTO.getCapacity();
-        if (capacity < 5 || capacity > 10) throw new CustomException(ErrorCode.CAPACITY_WRONG);
-
+    public ChatRoom createChatRoom(ChatRoomReqDTO chatRoomReqDTO) {
         // 채팅방 생성하기
-        ChatRoom savedChatRoom = chatRoomJpaRepository.save(createToEntity(chatRoomReqDTO));
-        // 응답 메시지 return
-        ResponseCode responseCode = ResponseCode.CHATROOM_CREATE;
-        return new ResponseEntity<>(ResResult.builder()
-                .responseCode(responseCode)
-                .code(responseCode.getCode())
-                .message(responseCode.getMessage())
-                .data(savedChatRoom)
-                .build(), HttpStatus.OK);
-
+        return chatRoomJpaRepository.save(createToEntity(chatRoomReqDTO));
     }
 }
