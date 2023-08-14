20d6c3600f93d8098914f5e47f2fda5ea0815d1d
ujumom <amethyst270@naver.com>
2023-08-14 09:14:36 +0900

    :sparkles: feat: <채팅방 생성 ver.2>

--- src/main/java/com/springles/controller/ChatRoomController.java
+++ src/main/java/com/springles/controller/ChatRoomController.java
@@ -5,10 +5,10 @@ import com.springles.domain.dto.request.ChatRoomReqDTO;
 import com.springles.domain.dto.response.ResResult;
 import com.springles.service.ChatRoomService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,8 +22,9 @@ public class ChatRoomController {
 
     // 채팅방 생성
     @Operation(summary = "채팅방 생성", description = "채팅방 생성")
-    @PostMapping(value = "/chatrooms", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResResult> createChatRoom(@RequestBody ChatRoomReqDTO chatRoomReqDTO){
+    @PostMapping(value = "/chatrooms", produces = "application/json;charset=UTF-8")
+    public ResponseEntity<ResResult> createChatRoom(@Valid @RequestBody ChatRoomReqDTO chatRoomReqDTO){
+
         // 응답 메시지 return
         ResponseCode responseCode = ResponseCode.CHATROOM_CREATE;
 
