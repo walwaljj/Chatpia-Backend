c84388e206ebeeb1a7f52c78cbc7a220ea032f1f
nayonnii <nyovv26@naver.com>
2023-08-16 10:20:22 +0900

    :recycle: refactor: validation 로직 @valid로 처리

--- src/main/java/com/springles/domain/dto/member/MemberResponse.java
+++ src/main/java/com/springles/domain/dto/member/MemberResponse.java
@@ -2,35 +2,42 @@ package com.springles.domain.dto.member;
 
 import com.springles.domain.entity.Member;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Builder
+@Getter
 public class MemberResponse {
 
     // 게임 아이디
-    @Getter
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Column(unique = true)
+    @Size(min = 6, max = 20, message = "아이디는 6 ~ 20자 사이여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문 대소문자, 숫자만 입력 할 수 있습니다.")
     private String memberName;
 
     // 비밃번호
-    @Getter
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 6, message = "비밀번호는 6자 이상이여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]+$", message = "비밀번호는 영문 대소문자, 숫자, 특수문자(!,@,#,$,%,^,&,*)만 입력할 수 있습니다.")
     private String password;
 
     // 비밃번호 확인
-    @Getter
     private String passwordConfirm;
 
     // 이메일
-    @Getter
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
     // 유저 역할 - Admin(관리자), User(일반유저)
-    @Getter
     private String role;
 
     // 탈퇴 여부
-    @Getter
     private Boolean isDeleted;
 
     public static MemberResponse fromEntity(Member entity) {
--- src/main/java/com/springles/domain/entity/Member.java
+++ src/main/java/com/springles/domain/entity/Member.java
@@ -1,6 +1,7 @@
 package com.springles.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -10,28 +11,27 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="member_id")
     private Long id;
 
-    // 이메일
-    private String email;
-
     // 게임 아이디
-    @Column(nullable = false, unique = true)
     private String memberName;
 
     // 비밃번호
-    @Column(nullable = false)
     private String password;
 
+    // 이메일
+    private String email;
+
     // 유저 역할 - Admin(관리자), User(일반유저)
-    @Column(nullable = false)
+    @NotNull
     private String role;
 
     // 탈퇴 여부
-    @Column(nullable = false)
+    @NotNull
     private Boolean isDeleted;
 
     @Override
