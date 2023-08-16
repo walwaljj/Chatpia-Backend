9462b308e15de8f04e4a3047c103ba3f47f2fb1c
dlrjs2360 <lhg3670@naver.com>
2023-08-16 11:12:46 +0900

    :sparkles: feat: 채팅방 ResponseDto 생성

--- src/main/java/com/springles/service/ChatRoomService.java
+++ src/main/java/com/springles/service/ChatRoomService.java
@@ -1,10 +1,11 @@
 package com.springles.service;
 
-import com.springles.domain.dto.request.ChatRoomReqDTO;
+import com.springles.domain.dto.chatroom.ChatRoomReqDTO;
+import com.springles.domain.dto.chatroom.ChatRoomResponseDto;
 import com.springles.domain.entity.ChatRoom;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface ChatRoomService {
     @Transactional
-    ChatRoom createChatRoom(ChatRoomReqDTO chatRoomReqDTO);
+    ChatRoomResponseDto createChatRoom(ChatRoomReqDTO chatRoomReqDTO);
 }
