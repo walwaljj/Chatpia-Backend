c7e9f21a3a7f99d18711633f5237cf3ae2f179f9
nayonnii <nyovv26@naver.com>
2023-08-18 14:29:57 +0900

    :recycle: refactor: 로그 및 미사용 코드 삭제

--- src/main/java/com/springles/jwt/JwtTokenFilter.java
+++ src/main/java/com/springles/jwt/JwtTokenFilter.java
@@ -1,7 +1,6 @@
 package com.springles.jwt;
 
 import com.springles.domain.dto.member.MemberCreateRequest;
-import com.springles.repository.BlackListTokenRedisRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,8 +17,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Optional;
 
 @Slf4j
 @Component
@@ -34,13 +31,10 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        log.info("필터 시작");
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.split(" ")[1];
-            log.info("필터 - 로그아웃 여부 체크");
             if (jwtTokenUtils.isNotLogout(token)) {
-                log.info("필터 - 토큰 유효성 검사");
                 if (jwtTokenUtils.validate(token)) {
                     SecurityContext context = SecurityContextHolder.createEmptyContext();
                     String memberName = jwtTokenUtils.parseClaims(token).getSubject();
@@ -56,7 +50,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 }
             }
         }
-        log.info("필터 끝");
         filterChain.doFilter(request, response);
     }
 }
