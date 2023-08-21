677b08b6f9b3e553a2b76f856830cb1c91b473bd
ujumom <amethyst270@naver.com>
2023-08-21 10:33:09 +0900

    :sparkles: feat: <dependencty setting, 기본 html 생성>

--- src/main/java/com/springles/controller/ui/ChatUiController.java
+++ src/main/java/com/springles/controller/ui/ChatUiController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("v1/chat")
 @Controller
 public class ChatUiController {
+    // 채팅방 들어갈 때
     @GetMapping("")
     public String enterRoom(
-           
             ){
         return "chat-room";
     }
+
 }
