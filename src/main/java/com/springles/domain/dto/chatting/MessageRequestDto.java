e7284d61fc0e432fc6adcad75e4b84619ebdc4b0
ranunclulus <morion002@gmail.com>
2023-08-21 14:06:45 +0900

    :sparkles: feat: <WebSocketMapping 설정> 실제로 메시지를 전송할 때 simpMessagingTemplate에 메시지를 전송하는 로직 구현 MessageDto에 시간 추가

--- src/main/java/com/springles/domain/dto/chatting/MessageRequestDto.java
+++ src/main/java/com/springles/domain/dto/chatting/MessageRequestDto.java
@@ -4,8 +4,10 @@ package com.springles.domain.dto.chatting;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class MessageRequestDto {
@@ -18,14 +20,16 @@ public class MessageRequestDto {
     private String message;
     private String sender;
     private String roomId;
+    private String time;
 
     @Override
     public String toString() {
         return "MessageRequestDto{" +
-            "type=" + type +
-            ", message='" + message + '\'' +
-            ", sender='" + sender + '\'' +
-            ", roomId=" + roomId +
+            "type =" + type +
+            ", message ='" + message + '\'' +
+            ", sender ='" + sender + '\'' +
+            ", time ='" + sender + '\'' +
+            ", roomId =" + time +
             '}';
     }
 }
