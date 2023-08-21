4a8f617b47a64af7ba96a363b5916c7922fa3932
ranunclulus <morion002@gmail.com>
2023-08-21 11:40:04 +0900

    :sparkles: feat: <Test를 위한 간단한 Ui Mapping Controller build.gradle에 GSON도 추가

---
+++ src/main/java/com/springles/controller/ui/ChatUiController.java
@@ -0,0 +1,27 @@
+package com.springles.controller.ui;
+
+import com.google.gson.Gson;
+import com.springles.websocket.SimpleChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("chat")
+@RequiredArgsConstructor
+public class ChatUiController {
+    private final SimpleChatHandler simpleChatHandler;
+    private final Gson gson;
+
+    @GetMapping("rooms")
+    public String rooms() {
+        return "rooms";
+    }
+
+    @GetMapping("enter")
+    public String enter(@RequestParam("username") String username) {
+        return "chat";
+    }
+}
