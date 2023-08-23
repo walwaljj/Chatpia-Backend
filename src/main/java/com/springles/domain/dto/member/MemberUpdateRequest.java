8f88a6e6600dc2c0324a7a6540ed8a32a9443c55
nayonnii <nyovv26@naver.com>
2023-08-24 01:24:13 +0900

    :bug: fix: 테스트코드 오류 수정(생성자 어노테이션 추가)

--- src/main/java/com/springles/domain/dto/member/MemberUpdateRequest.java
+++ src/main/java/com/springles/domain/dto/member/MemberUpdateRequest.java
@@ -5,11 +5,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberUpdateRequest {
 
     // 게임 아이디
