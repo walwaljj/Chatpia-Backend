14726e7c07976a91f198fa10cdbfd40160d00e32
nayonnii <nyovv26@naver.com>
2023-08-25 19:15:42 +0900

    :bug: fix: @setter 추가

--- src/main/java/com/springles/domain/dto/member/MemberProfileCreateRequest.java
+++ src/main/java/com/springles/domain/dto/member/MemberProfileCreateRequest.java
@@ -6,13 +6,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberProfileCreateRequest {
--- src/main/java/com/springles/domain/dto/member/MemberUpdateRequest.java
+++ src/main/java/com/springles/domain/dto/member/MemberUpdateRequest.java
@@ -4,13 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberUpdateRequest {
