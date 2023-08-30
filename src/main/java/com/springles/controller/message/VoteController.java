ba0f50a54d7fcab9ecb62b5d4f3f479006028dd4
ranunclulus <morion002@gmail.com>
2023-08-30 11:25:28 +0900

    :sparkles: feat: <투표를 확정하는 메소드>

--- src/main/java/com/springles/controller/message/VoteController.java
+++ src/main/java/com/springles/controller/message/VoteController.java
@@ -1,7 +1,9 @@
 package com.springles.controller.message;
 
+import com.springles.domain.dto.vote.ConfirmResultResponseDto;
 import com.springles.domain.dto.vote.GameSessionVoteRequestDto;
 import com.springles.domain.dto.vote.VoteResultResponseDto;
+import com.springles.domain.entity.GameSession;
 import com.springles.exception.CustomException;
 import com.springles.exception.constants.ErrorCode;
 import com.springles.game.ChatMessage;
@@ -17,6 +19,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -41,6 +44,38 @@ public class VoteController {
             simpMessagingTemplate.convertAndSend("/sub/chat/" + roomId,
                     VoteResultResponseDto.of(voteResult));
         }
+    }
 
+    @MessageMapping("/pub/chat/{roomId}/confirm")
+    private void confirmVote(SimpMessageHeaderAccessor accessor,
+                         @DestinationVariable Long roomId,
+                         @Payload GameSessionVoteRequestDto request) {
+        String userName = accessor.getUser().getName();
+        Long playerId = gameSessionManager.findMemberByMemberName(userName).getId();
+
+        GameSession gameSession = gameSessionManager.findGameByRoomId(roomId);
+
+        Map<Long, Boolean> confirmResult = gameSessionVoteService.confirmVote(roomId, playerId, request);
+
+        if(confirmResult.size() <= 0) {
+            throw new CustomException(ErrorCode.FAIL_CONFIRM_VOTE);
+        }
+        else {
+            simpMessagingTemplate.convertAndSend("/sub/chat/" + roomId,
+                    ConfirmResultResponseDto.of(confirmResult));
+
+            int confirmCnt = confirmResult.entrySet().stream()
+                    .filter(e -> e.getValue() == true) // confirm == true인 이용자
+                    .collect(Collectors.toList()).size();
+
+            int alivePlayerCnt = gameSession.getAliveCivilian()
+                    + gameSession.getAliveDoctor()
+                    + gameSession.getAlivePolice()
+                    + gameSession.getAliveMafia();
+
+            if (confirmCnt == alivePlayerCnt) { // 살아 있는 모두가 투표를 끝내면 투표 종료
+                gameSessionVoteService.endVote(roomId, gameSession.getPhaseCount(), request.getPhase());
+            }
+        }
     }
 }
