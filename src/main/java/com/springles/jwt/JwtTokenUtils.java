2b69659dcb562dcf916bcc0464abc6abb08e2c1e
nayonnii <nyovv26@naver.com>
2023-09-01 14:46:34 +0900

    :bug: fix: accessToken reissue 메소드 반환값 변경

--- src/main/java/com/springles/jwt/JwtTokenUtils.java
+++ src/main/java/com/springles/jwt/JwtTokenUtils.java
@@ -74,13 +74,16 @@ public class JwtTokenUtils {
         // refreshToken이 있는지 확인
         Optional<RefreshToken> optionalRefreshToken = refreshTokenRedisRepository.findById(refreshTokenId);
         if(optionalRefreshToken.isEmpty()) {
-            log.warn("refresh token이 존재하지 않음");
-            throw new CustomException(ErrorCode.NO_JWT_TOKEN);
+            log.warn("refresh token이 DB에 존재하지 않음");
+//            throw new CustomException(ErrorCode.NO_JWT_TOKEN);
+            return "";
         }
 
         // refreshToken이 유효한지 확인
         if(!memberJpaRepository.existsByMemberName(optionalRefreshToken.get().getMemberName())) {
-            throw new CustomException(ErrorCode.INVALID_REFRESH_TOKEN);
+            log.warn("refresh token이 유효하지 않음");
+//            throw new CustomException(ErrorCode.INVALID_REFRESH_TOKEN);
+            return "";
         }
 
         // accessToken 재발급
