6ef584b9c57d529791ac6326d010a2ed3cf00c00
nayonnii <nyovv26@naver.com>
2023-09-04 17:21:22 +0900

    :sparkles: feat: 로그인 예외 처리 코드 추가

--- src/main/java/com/springles/domain/dto/member/MemberLoginRequest.java
+++ src/main/java/com/springles/domain/dto/member/MemberLoginRequest.java
@@ -12,5 +12,6 @@ public class MemberLoginRequest {
 
     @NotBlank(message = "아이디를 입력해주세요.")
     private String memberName;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
