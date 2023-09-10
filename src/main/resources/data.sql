b9b9584b76a5af1e8f0acfd31f65009645d99455
dlrjs2360 <lhg3670@naver.com>
2023-09-10 17:41:59 +0900

    :truck: chore: 더미데이터 sql 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,9 @@
 insert into member
 (email,is_deleted,member_name,password,role)
 values
-    ('admin@naver.com',0,'admin123','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
     ('test1@naver.com',0,'test1','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
     ('test2@naver.com',0,'test2','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
-    ('test3@naver.com',0,'test4','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
+    ('test3@naver.com',0,'test3','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
     ('test4@naver.com',0,'test4','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
     ('test5@naver.com',0,'test5','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
     ('test6@naver.com',0,'test6','$2a$10$jWpTgBFPm4f77Jklchp7iu/oe0uaB8VeaeBiEueRp3/xOlFLvkPqC', 'USER'),
@@ -15,8 +14,8 @@ values
 insert into member_game_info
 (is_observer, exp, member_game_info_id, member_id, in_game_role, level, nickname, profile_img)
 values
-    (0,'0','1','1','NONE','BEGINNER','admin','PROFILE01'),
-    (0,'0','2','2','NONE','BEGINNER','test1','PROFILE01'),
+    (0,'0','1','1','NONE','BEGINNER','test1','PROFILE01'),
+    (0,'0','2','2','NONE','BEGINNER','test2','PROFILE01'),
     (0,'0','3','3','NONE','BEGINNER','test3','PROFILE01'),
     (0,'0','4','4','NONE','BEGINNER','test4','PROFILE01'),
     (0,'0','5','5','NONE','BEGINNER','test5','PROFILE01'),
@@ -24,15 +23,15 @@ values
     (0,'0','7','7','NONE','BEGINNER','test7','PROFILE01'),
     (0,'0','8','8','NONE','BEGINNER','test8','PROFILE01');
 
-insert into game_record
+/*insert into game_record
 (title, owner_id, state, capacity, head, open, winner, duration)
 values
     ('testTitle1', 1, '진행중', 10, '9', true, true, 60),
     ('testTitle1', 1, '진행중', 10, '9', true, true, 60),
     ('testTitle1', 1, '진행중', 10, '9', true, true, 60),
-    ('testTitle1', 1, '진행중', 10, '9', true, true, 60);
+    ('testTitle1', 1, '진행중', 10, '9', true, true, 60);*/
 
-insert into member_record(
+/*insert into member_record(
     mafia_cnt, citizen_cnt, doctor_cnt, police_cnt,
     save_cnt, kill_cnt,
     mafia_win_cnt, citizen_win_cnt, police_win_cnt, doctor_win_cnt,
@@ -42,4 +41,4 @@ values
     (5, 10, 20, 10, 5, 5, 2, 5, 4, 10, 100, 1000, 1),
     (5, 10, 20, 10, 5, 5, 2, 5, 4, 10, 100, 1000, 2),
     (5, 10, 20, 10, 5, 5, 2, 5, 4, 10, 100, 1000, 3),
-    (5, 10, 20, 10, 5, 5, 2, 5, 4, 10, 100, 1000, 4);
+    (5, 10, 20, 10, 5, 5, 2, 5, 4, 10, 100, 1000, 4);*/
