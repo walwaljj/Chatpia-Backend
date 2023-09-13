d2510b0f2852aa09befea39449184e550f470291
ujumom <amethyst270@naver.com>
2023-09-13 11:05:10 +0900

    :sparkles: feat: <참여자 목록 디자인 수정 ver. 2, 이미지 경로 수정>

--- src/main/java/com/springles/domain/constants/ProfileImg.java
+++ src/main/java/com/springles/domain/constants/ProfileImg.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum ProfileImg {
-    PROFILE01("../images/profile_01.jpg"),
-    PROFILE02("../images/profile_02.jpg"),
-    PROFILE03("../images/profile_03.jpg"),
-    PROFILE04("../images/profile_04.jpg"),
-    PROFILE05("../images/profile_05.jpg"),
-    PROFILE06("../images/profile_06.jpg");
+//    PROFILE01("../images/profile_01.jpg"),
+//    PROFILE02("../images/profile_02.jpg"),
+//    PROFILE03("../images/profile_03.jpg"),
+//    PROFILE04("../images/profile_04.jpg"),
+//    PROFILE05("../images/profile_05.jpg"),
+//    PROFILE06("../images/profile_06.jpg");
+
+    PROFILE01("/images/profile_01.jpg"),
+    PROFILE02("/images/profile_02.jpg"),
+    PROFILE03("/images/profile_03.jpg"),
+    PROFILE04("/images/profile_04.jpg"),
+    PROFILE05("/images/profile_05.jpg"),
+    PROFILE06("/images/profile_06.jpg");
+
 
     private String fileUrl;
 
